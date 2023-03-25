76fd1af37c56d62c99d2814bf081777b4eaa1930
antonepifanov <antonepifanov87@gmail.com>
2023-03-25 18:54:51 +0300

    Исправляет опечатки

--- source/js/modules/init-tabs.js
+++ source/js/modules/init-tabs.js
@@ -2,7 +2,7 @@ import Tabs from '../vendor/tabs';
 
 export const getThemesTabs = () => {
   return new Tabs('questions', {
-    activeButtonClass: 'quections__nav-button--active',
-    activePanelClass: 'quections__content-item--active',
+    activeButtonClass: 'questions__nav-button--active',
+    activePanelClass: 'questions__content-item--active',
   });
 };
